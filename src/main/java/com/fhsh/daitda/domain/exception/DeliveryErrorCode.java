c9385431981a91a80a15d53a1873119b0b0b489e
Yoonho Cha <0119hoya@naver.com>
2026-04-03 01:29:14 +0900

    feat: 비지니스별 에러코드 적용을 위한 파일 생성 및 에러코드 작성

---
+++ src/main/java/com/fhsh/daitda/domain/exception/DeliveryErrorCode.java
@@ -0,0 +1,15 @@
+package com.fhsh.daitda.domain.exception;
+
+import com.fhsh.daitda.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum DeliveryErrorCode implements ErrorCode {
+    NOT_FOUND_DELIVERY(HttpStatus.NOT_FOUND, "배송을 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String description;
+}
