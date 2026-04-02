51632617574ad58b7d795e4dd699f6239bd9501b
Yoonho Cha <0119hoya@naver.com>
2026-04-03 01:26:10 +0900

    feat: repository 작성(초안)

--- src/main/java/com/fhsh/daitda/domain/repository/DeliveryRepository.java
+++ src/main/java/com/fhsh/daitda/domain/repository/DeliveryRepository.java
@@ -1,4 +1,16 @@
 package com.fhsh.daitda.domain.repository;
 
+import com.fhsh.daitda.domain.entity.Delivery;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
+import java.util.Optional;
+import java.util.UUID;
+
 public interface DeliveryRepository {
+    Optional<Delivery> findById(UUID deliveryId);
+    Slice<Delivery> findAll(Pageable pageable);
+    Slice<Delivery> findByDeliveryManagerId(UUID deliveryManagerId, Pageable pageable);
+    Slice<Delivery> findByHubId(UUID hubId, Pageable pageable);
+
 }
--- src/main/java/com/fhsh/daitda/infrastructure/DeliveryRepositoryImpl.java
+++ src/main/java/com/fhsh/daitda/infrastructure/DeliveryRepositoryImpl.java
@@ -1,6 +1,40 @@
 package com.fhsh.daitda.infrastructure;
 
+import com.fhsh.daitda.domain.entity.Delivery;
 import com.fhsh.daitda.domain.repository.DeliveryRepository;
+import com.fhsh.daitda.infrastructure.persistence.repository.DeliveryJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
 public class DeliveryRepositoryImpl implements DeliveryRepository {
+
+    private final DeliveryJpaRepository deliveryJpaRepository;
+
+    @Override
+    public Slice<Delivery> findAll(Pageable pageable) {
+        return deliveryJpaRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Delivery> findById(UUID deliveryId) {
+        return deliveryJpaRepository.findById(deliveryId);
+    }
+
+    @Override
+    public Slice<Delivery> findByDeliveryManagerId(UUID deliveryManagerId, Pageable pageable) {
+        return deliveryJpaRepository.findByDeliveryManagerId(deliveryManagerId, pageable);
+    }
+
+    @Override
+    public Slice<Delivery> findByHubId(UUID hubId, Pageable pageable) {
+        return deliveryJpaRepository.findByHubId(hubId, pageable);
+    }
 }
