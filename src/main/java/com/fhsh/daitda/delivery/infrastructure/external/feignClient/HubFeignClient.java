3409c4751e1b7fdf6b41f9d4ab535fd4b50fa47a
Yoonho Cha <0119hoya@naver.com>
2026-04-03 23:57:56 +0900

    feat: FeignClient로 외부 서비스와 통신하기 위한 feignClient 작성

---
+++ src/main/java/com/fhsh/daitda/delivery/infrastructure/external/feignClient/HubFeignClient.java
@@ -0,0 +1,17 @@
+package com.fhsh.daitda.delivery.infrastructure.external.feignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
+import java.util.UUID;
+
+@FeignClient(name = "hub-service")
+public interface HubFeignClient {
+
+    @GetMapping("/internal/v1/hubs/{userId}")
+    Map<UUID, String> getHubIdByManagerId(@PathVariable("userId") UUID userId);
+
+
+}
