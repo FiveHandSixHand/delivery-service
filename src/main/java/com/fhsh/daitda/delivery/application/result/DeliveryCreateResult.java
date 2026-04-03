8fef8ca635c6f38d0ac3a35d5aada9eb5ed952c5
Yoonho Cha <0119hoya@naver.com>
2026-04-04 00:00:10 +0900

    feat: ResultDTO 작성

---
+++ src/main/java/com/fhsh/daitda/delivery/application/result/DeliveryCreateResult.java
@@ -0,0 +1,40 @@
+package com.fhsh.daitda.delivery.application.result;
+
+import com.fhsh.daitda.delivery.domain.entity.Delivery;
+import com.fhsh.daitda.delivery.domain.enums.DeliveryStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class DeliveryCreateResult {
+
+    private UUID deliveryId;
+    private UUID orderId;
+    private DeliveryStatus status;
+    private UUID srcHubId;
+    private UUID destHubId;
+    private String senderTenantAddress;
+    private String receiverTenantAddress;
+    private UUID receiverId;
+    private UUID deliveryManagersId;
+    private LocalDateTime createdAt;
+
+    public static DeliveryCreateResult from(Delivery delivery) {
+        return new DeliveryCreateResult(
+                delivery.getId(),
+                delivery.getOrderId(),
+                delivery.getStatus(),
+                delivery.getDepartureHubId(),
+                delivery.getDestinationHubId(),
+                delivery.getSenderTenantAddress(),
+                delivery.getReceiverTenantAddress(),
+                delivery.getReceiverId(),
+                delivery.getDeliveryManagerId(),
+                delivery.getCreatedAt()
+        );
+    }
+}
