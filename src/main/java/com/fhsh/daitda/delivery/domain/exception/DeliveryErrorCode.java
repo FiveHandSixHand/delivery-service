b275a5c68f8dd01029d2ed9f4dae5c823cbacea6
Yoonho Cha <0119hoya@naver.com>
2026-04-04 22:17:55 +0900

    fix: delivery error 코드 추가

--- src/main/java/com/fhsh/daitda/delivery/domain/exception/DeliveryErrorCode.java
+++ src/main/java/com/fhsh/daitda/delivery/domain/exception/DeliveryErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum DeliveryErrorCode implements ErrorCode {
-    NOT_FOUND_DELIVERY(HttpStatus.NOT_FOUND, "배송을 찾을 수 없습니다.");
+    NOT_FOUND_DELIVERY(HttpStatus.NOT_FOUND, "배송을 찾을 수 없습니다."),
+    INVALID_STATUS_TRANSITION(HttpStatus.BAD_REQUEST, "유효하지 않은 배송 상태 전환입니다."),
+    CANNOT_CANCEL_DELIVERED(HttpStatus.BAD_REQUEST, "이미 완료된 배송은 취소할 수 없습니다.");
 
     private final HttpStatus status;
     private final String description;
