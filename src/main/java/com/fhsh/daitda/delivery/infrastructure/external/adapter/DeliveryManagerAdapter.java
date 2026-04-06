6e96270d71c4d4b597c38d10fe590a01179f2bfb
Yoonho Cha <0119hoya@naver.com>
2026-04-06 12:13:33 +0900

    refact: 충돌 해결

--- src/main/java/com/fhsh/daitda/delivery/infrastructure/external/adapter/DeliveryManagerAdapter.java
+++ src/main/java/com/fhsh/daitda/delivery/infrastructure/external/adapter/DeliveryManagerAdapter.java
@@ -3,7 +3,6 @@ package com.fhsh.daitda.delivery.infrastructure.external.adapter;
 import com.fhsh.daitda.delivery.application.client.DeliveryManagerClient;
 import com.fhsh.daitda.delivery.infrastructure.external.dto.DeliveryManagerAssignRequest;
 import com.fhsh.daitda.delivery.infrastructure.external.feignClient.DeliveryManagerFeignClient;
-import com.fhsh.daitda.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +15,12 @@ public class DeliveryManagerAdapter implements DeliveryManagerClient {
     private final DeliveryManagerFeignClient deliveryManagerFeignClient;
 
     @Override
-    public CommonResponse<UUID> assignHubDeliveryManager(UUID companyId) {
+    public UUID assignHubDeliveryManager(UUID companyId) {
         return null;
     }
 
     @Override
-    public CommonResponse<UUID> assignCompanyDeliveryManager(UUID deliveryId, UUID hubId) {
+    public UUID assignCompanyDeliveryManager(UUID deliveryId, UUID hubId) {
         return deliveryManagerFeignClient.assignCompanyDeliveryManager(new DeliveryManagerAssignRequest(deliveryId, hubId));
     }
 }
